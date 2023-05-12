4b8fdfb5367ce6a006a6d65560d53389eb3c5154
Dmitry <dmitrypolyakov1985@gmail.com>
2023-05-12 12:31:30 -0600

    mongoose stuff for fetching products finished

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 5000;
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB();
 const app = express();
@@ -12,13 +14,9 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((prod) => prod._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is up on ${port}`));
